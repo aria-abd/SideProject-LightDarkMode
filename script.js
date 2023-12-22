e98ade4ce30b66f46f37d4e78d4f796d044591e0
Aria <montaghed.man@gmail.com>
2023-12-22 23:12:37 +0330

    Light-Dark-Mode

---
+++ script.js
@@ -0,0 +1,57 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+// Dark or Light Images
+function imageMode(props) {
+  image1.src = `img/undraw_proud_coder_${props}.svg`;
+  image2.src = `img/undraw_feeling_proud_${props}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${props}.svg`;
+}
+//  Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  toggleIcon.children[0].textContent = "Drak Mode";
+  toggleIcon.children[1].classList.replace("bi-sun", "bi-moon");
+  imageMode("dark");
+}
+//  Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("bi-moon", "bi-sun");
+  imageMode("light");
+}
+
+// Switch theme Dynamically
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    lightMode();
+  }
+}
+
+// Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
